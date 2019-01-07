499fcf98e2543e34e7cf699e5423d9f013b7fcf1
umaumax <umauma7083@gmail.com>
2019-01-07 23:33:51 +0900

    Add wcat command

---
+++ README.md
@@ -0,0 +1,15 @@
+# wcat
+
+Wrapper of cat
+
+## how to install
+```
+cd ~/local/bin
+wget https://raw.githubusercontent.com/umaumax/wcat/master/wcat.sh -O wcat
+chmod u+x wcat
+```
+
+## how to use
+```
+wcat $filepath:$line:$range
+```
---
+++ wcat.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+{ [[ $1 == '-h' ]] || [[ $1 == '-help' ]] || [[ $1 == '--help' ]]; } && echo "$0 "'$filepath:$line:$range' && exit 1
+
+function cmdcheck() {
+	type -a "$1" >/dev/null 2>&1
+}
+
+function main() {
+	local file_path="${1%%:*}"
+	local line_no=$(echo "$1" | awk -F':' '{print $2}')
+	if [[ -z $line_no ]]; then
+		local line_no=0
+	fi
+	local range=$(echo "$1" | awk -F':' '{print $3}')
+	if [[ -z $range ]]; then
+		local range=5
+	fi
+
+	local CAT='cat -n'
+	local pre_line=0
+	if cmdcheck bat; then
+		# NOTE: bat has --line-range <start>:<end> option
+		local CAT='bat --color=always'
+		local pre_line=3
+	elif cmdcheck ccat; then
+		local CAT='ccat -C=always'
+		local pre_line=0
+	fi
+
+	eval $CAT $file_path | awk -v base=$line_no -v range=$range -v pre_line=$pre_line '(pre_line+base-range)<=NR && NR<=(pre_line+base+range)'
+}
+
+main "$@"
